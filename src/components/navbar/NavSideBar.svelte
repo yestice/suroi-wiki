89de1fc750991109d815818b5a92713fd77cad84
Kenos <75338427+kenos1@users.noreply.github.com>
2023-11-18 14:33:23 -0700

    change font and add sidebar

---
+++ src/components/navbar/NavSideBar.svelte
@@ -0,0 +1,26 @@
+<script lang="ts"></script>
+
+<div class="flex sticky max-h-screen top-0 bottom-0 left-0 bg-background border-border border-r-2 p-4 text-lg text-foreground">
+  <ul class="flex flex-col gap-4 p-4">
+    <li class="mb-4">
+      <a href="/">
+        <img src="/img/logo.svg" width=150 />
+      </a>
+    </li>
+    <li>
+      <a href="/">Home</a>
+    </li>
+    <li>
+      <a href="/weapons">Weapons</a>
+    </li>
+    <li>
+      <a href="/consumables">Consumables</a>
+    </li>
+    <li>
+      <a href="/loottables">Loot Tables</a>
+    </li>
+    <li>
+      <a href="/equipment">Equipment</a>
+    </li>
+  </ul>
+</div>
