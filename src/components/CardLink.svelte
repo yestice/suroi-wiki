37f65a71c77dea011c6e4fc16aab258be40e8df4
Kenos <75338427+kenos1@users.noreply.github.com>
2023-11-18 11:52:44 -0700

    Added homepage and weapons page

---
+++ src/components/CardLink.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  export let title: string;
+  export let url: string;
+  export let description: string;
+  export let image: string;
+</script>
+
+<div class="flex-1">
+  <a href={url}>
+    <div
+      class="flex gap-8 p-4 w-full h-full rounded-md border transition-colors border-border hover:border-blue-500 group"
+    >
+      <div class="flex justify-center items-center">
+        <img
+          src={image}
+          width={100}
+          height={100}
+          alt={`${title} page image`}
+          class=""
+        />
+      </div>
+      <div class="flex-1">
+        <h3
+          class="text-lg font-bold leading-loose underline transition-colors group-hover:text-blue-500"
+        >
+          {title}
+        </h3>
+        <p class="transition-colors group-hover:text-blue-500">
+          {description}
+        </p>
+      </div>
+    </div>
+  </a>
+</div>
