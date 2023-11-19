c4bc3a8583b28d4c8bd78acf6516160690f4cd6d
Kenos <75338427+kenos1@users.noreply.github.com>
2023-11-19 10:36:07 -0700

    Migrate icons to svelte

---
+++ src/components/icons/Emote.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let id: string;
+</script>
+
+<img
+  src={"../../../vendor/suroi/client/public/img/game/emotes/" + id + ".svg"}
+  alt={id + " icon"}
+  width=75
+  height=75
+/>
---
+++ src/components/icons/LootIcon.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let id: string;
+</script>
+
+<img
+  src={"../../../vendor/suroi/client/public/img/game/loot/" + id + ".svg"}
+  alt={id + " icon"}
+  width=50
+  height=50
+/>
