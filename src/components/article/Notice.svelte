0eab8fd009446b678f8b27cb112dec9bd869e20e
Kenos <75338427+kenos1@users.noreply.github.com>
2023-11-19 10:35:47 -0700

    Add notice component

---
+++ src/components/article/Notice.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import Emote from "../icons/Emote.svelte";
+
+  export let text: string;
+  export let emoteID: string;
+</script>
+
+<div class="flex gap-4 items-center p-2 pl-4 bg-muted">
+  <div class="flex items-center">
+    <Emote id={emoteID} />
+  </div>
+  <div class="flex flex-1 justify-center">
+    <span class="font-bold">
+      {text}
+    </span>
+  </div>
+</div>
