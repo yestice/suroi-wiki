d0901672be38d8cef2f01f01b144387211bad06a
Kenos <75338427+kenos1@users.noreply.github.com>
2023-11-23 17:49:18 -0700

    Add melee pages

---
+++ src/components/sidebars/MeleeSidebar.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import type { MeleeDefinition } from "@suroi/common/src/definitions/melees";
+  import BaseSidebar from "./util/BaseSidebar.svelte";
+  import Section from "./util/Section.svelte";
+  import SectionImage from "./util/SectionImage.svelte";
+  import SectionValue from "./util/SectionValue.svelte";
+  import SectionRow from "./util/SectionRow.svelte";
+
+  export let obj: MeleeDefinition;
+
+  const dps: number = obj.damage * (1000 / obj.cooldown);
+</script>
+
+<BaseSidebar>
+  <div class="flex flex-col">
+    <Section title={obj.name}>
+      <SectionImage
+        img={"../../../vendor/suroi/client/public/img/game/weapons/" +
+          obj.idString +
+          ".svg"}
+        alt={obj.name + " Image"}
+      />
+      <SectionValue name="ID" value={obj.idString} mono={true} />
+      <SectionRow>
+        <SectionValue name="Damage" value={obj.damage.toString()} />
+        <SectionValue
+          name="Obstacle Multiplier"
+          value={obj.obstacleMultiplier * 100 + "%"}
+        />
+        <SectionValue name="Cooldown" value={obj.cooldown + " ms"} />
+      </SectionRow>
+      <SectionRow>
+        <SectionValue name="DPS" value={dps.toFixed(2)} />
+        <SectionValue
+          name="Obstacle DPS"
+          value={(dps * obj.obstacleMultiplier).toFixed(2)}
+        />
+      </SectionRow>
+    </Section>
+  </div>
+</BaseSidebar>
