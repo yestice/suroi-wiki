977d30eb0ee1a93330d269d8b989c82249a75287
Kenos <75338427+kenos1@users.noreply.github.com>
2023-11-25 13:22:29 -0700

    Compositor's improved sidebars

--- src/components/sidebars/util/SectionGrid.svelte
+++ src/components/sidebars/util/SectionGrid.svelte
@@ -3,7 +3,8 @@
 </script>
 
 <div
-  style="display: grid; grid-template-columns: repeat({columns}, minmax(0, 1fr))" class="divide-x divide-primary border-primary border-t box-border"
+  style="display: grid; grid-template-columns: repeat({columns}, minmax(0, 1fr))"
+  class="divide-x divide-primary border-primary border-t box-border"
 >
   <slot />
 </div>
