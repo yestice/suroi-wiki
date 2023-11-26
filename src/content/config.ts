54f7850a08cc93c929080e554d719a82eb5d1688
Compositr <compositr@compositr.dev>
2023-11-26 12:47:37 +1100

    chore: add config file for typings and BP

---
+++ src/content/config.ts
@@ -0,0 +1,29 @@
+import { defineCollection, z } from "astro:content";
+
+export const collections = {
+  buildings: defineCollection({
+    type: "content",
+  }),
+  consumables: defineCollection({
+    type: "content",
+  }),
+  guns: defineCollection({
+    type: "content",
+  }),
+  melees: defineCollection({
+    type: "content",
+  }),
+
+  community: defineCollection({
+    type: "content",
+    schema: z.object({
+      title: z.string(),
+      tags: z.array(z.string()),
+      author: z.string(),
+      pinned: z.optional(z.boolean()),
+
+      published: z.coerce.date(z.string().regex(/\d{4}-\d{2}-\d{2}/)),
+      updated: z.coerce.date(z.string().regex(/\d{4}-\d{2}-\d{2}/)),
+    }),
+  }),
+};
