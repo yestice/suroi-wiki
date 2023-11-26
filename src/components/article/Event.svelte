0d3308c61bbbfb1b206a88978d24c16fcf0a3b29
Compositr <compositr@compositr.dev>
2023-11-27 07:04:32 +1100

    fix: temporary fixes for build not passing - put og images on hold - remove imports from troublesome mdx files

---
+++ src/components/article/Event.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import Notice from "./Notice.svelte";
+</script>
+
+<Notice text="This article is about gameplay or items only obtainable during special events." emoteID="upside_down_face" />
