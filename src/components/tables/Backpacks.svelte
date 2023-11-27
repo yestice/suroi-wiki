0911cfc0b104ecd06a9fb2c0ab4da3a747e6dea3
Kenos <75338427+kenos1@users.noreply.github.com>
2023-11-26 20:04:31 -0700

    Backpacks table

---
+++ src/components/tables/Backpacks.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { Backpacks } from "@suroi/common/src/definitions/backpacks";
+</script>
+
+<table class="table-fixed">
+  <thead>
+    <tr>
+      <th></th>
+      {#each Object.entries(Backpacks.definitions[0].maxCapacity) as capacity}
+        <th>{capacity[0]}</th>
+      {/each}
+    </tr>
+  </thead>
+  <tbody>
+    {#each Backpacks.definitions as backpack}
+      <tr>
+        <td>{backpack.name}</td>
+        {#each Object.entries(backpack.maxCapacity) as capacity}
+          <td>{capacity[1]}</td>
+        {/each}
+      </tr>
+    {/each}
+  </tbody>
+</table>
