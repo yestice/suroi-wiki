6c7a75a5dabc4452659d68b9331030a3482aad02
Kenos <75338427+kenos1@users.noreply.github.com>
2023-11-27 21:18:54 -0700

    wip: table of contents

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,24 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import mdx from "@astrojs/mdx";
 
 import react from "@astrojs/react";
 
+import rehypeSlug from "rehype-slug"
+import rehypeParse from "rehype-parse"
+import rehypeStringify from "rehype-stringify"
+import rehypeToc from "rehype-toc"
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), mdx(), react()]
+  integrations: [
+    tailwind(),
+    svelte(),
+    mdx(),
+    react(),
+  ],
+  markdown: {
+    rehypePlugins: [rehypeSlug, rehypeParse, rehypeStringify, rehypeToc]
+  }
 });
