afd651640e0baf7b4920e6322de9adf95b03271e
Kenos <75338427+kenos1@users.noreply.github.com>
2023-11-27 18:57:19 -0700

    Add dev weapon notices

---
+++ src/components/article/DevWeapon.svelte
@@ -0,0 +1,5 @@
+<script>
+  import Notice from "./Notice.svelte";
+</script>
+
+<Notice emoteID="bleh" text="This article is about a developer weapon, meaning it doesn't appear in normal lobbies" />
