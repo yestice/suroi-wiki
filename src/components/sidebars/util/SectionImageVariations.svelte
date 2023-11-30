a8a0234e8f2e9e0d08f61ec7230550b6e2ce61e2
Compositr <compositr@compositr.dev>
2023-12-01 00:11:50 +1100

    wip: obstacle pages

---
+++ src/components/sidebars/util/SectionImageVariations.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  export let alt: string;
+  export let imgs: string[];
+
+  let index = 0;
+  $: img = imgs[index];
+
+  function set(num: number) {
+    index = num;
+  }
+</script>
+
+<div class="flex flex-col flex-1 items-center gap-4">
+  <img src={img} {alt} width="150" height="150" class="flex-1" />
+  <div class="flex justify-around w-full">
+    {#each imgs as _, i}
+      <button
+        class={`text-lg p-2 rounded-md border ${
+          i === index ? "border-primary bg-primary" : "hover:text-primary"
+        } transition-colors hover:border-primary `}
+        on:click={() => set(i)}>{i + 1}</button
+      >
+    {/each}
+  </div>
+</div>
--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,7 +1,13 @@
+import { Loots } from "@suroi/common/src/definitions/loots";
+
 //https://stackoverflow.com/a/10050831
 export function range(
   size: number,
   startAt: number = 0
 ): ReadonlyArray<number> {
   return [...Array(size).keys()].map((i) => i + startAt);
+}
+
+export function findSuroiItem(idString: string) {
+  return Loots.definitions.find((item) => item.idString === idString);
 }
